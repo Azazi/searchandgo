8ef08ac3ff0e8b90d7d8fbd8bea75d70f9bb7299
Gellert Kispal <kispalgellert@yahoo.com>
2013-12-15 14:08:11 -0700

    Fixed find at other locations map bug and added more maps

--- SearchAndGo/FindAtOtherLocation.xaml.cs
+++ SearchAndGo/FindAtOtherLocation.xaml.cs
@@ -24,11 +24,34 @@ namespace SearchAndGo
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, RoutedEventArgs e)
+        private void South_Click(object sender, RoutedEventArgs e)
         {
-            mapToSouthcenter.Height = 700;
-            mapToSouthcenter.Width = 700;           
-            mapToSouthcenter.Source = (ImageSource)new ImageSourceConverter().ConvertFromString(@"../../Resources/Images/streetMap.png");
+            if (mapToSouthcenter.Height != 0)
+            {
+                mapToSouthcenter.Height = 0;
+                mapToSouthcenter.Width = 0;
+            }
+            else
+            {
+                mapToSouthcenter.Height = 642;
+                mapToSouthcenter.Width = 642;
+                mapToSouthcenter.Source = (ImageSource)new ImageSourceConverter().ConvertFromString(@"../../Resources/Images/imageMapToSouthCenter.png");
+            }
+        }
+
+        private void Market_Click(object sender, RoutedEventArgs e)
+        {
+            if (mapToMarket.Height != 0)
+            {
+                mapToMarket.Height = 0;
+                mapToMarket.Width = 0;
+            }
+            else
+            {
+                mapToMarket.Height = 642;
+                mapToMarket.Width = 642;
+                mapToMarket.Source = (ImageSource)new ImageSourceConverter().ConvertFromString(@"../../Resources/Images/imageMapToMarketMall.png");
+            }
         }
 
         private void closeButton_Click(object sender, RoutedEventArgs e)
